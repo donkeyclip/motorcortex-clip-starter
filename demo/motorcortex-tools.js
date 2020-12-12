44504b9e300341a1edff38e44b925c22726e31d4
Andreas Trantidis <atrantidis@gmail.com>
2020-12-12 22:08:32 +0000

    disable account menu when not logged in

--- demo/motorcortex-tools.js
+++ demo/motorcortex-tools.js
@@ -1,7 +1,7 @@
 import {clip, initParamsMap} from "../clip/clip";
 import Player from "@kissmybutton/motorcortex-player";
 
-const isLoggedIn = true;
+const isLoggedIn = false;
 
 const clipContainer = document.getElementById('clip');
 const toolset = document.getElementById('motorcortex-tools');
