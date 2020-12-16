589e4e4eebd779e299a8c99534f40cdc119991ac
Andreas Trantidis <atrantidis@gmail.com>
2020-12-16 09:23:10 +0000

    progress on json editor

--- demo/motorcortex-tools.js
+++ demo/motorcortex-tools.js
@@ -7,10 +7,25 @@ const isLoggedIn = true;
 
 const clipContainer = document.getElementById('clip');
 const toolset = document.getElementById('motorcortex-tools');
-const editor = document.getElementById('editor');
-// const logo = document.getElementById('motorcortex-logo');
-// const menu = document.getElementById('menu');
+const editor = document.getElementById('json-editor');
+const closeEditor = document.getElementById('cancel-init-params');
+const saveParams = document.getElementById('save-init-params');
 const initParamsSetButton = document.getElementById('set-init-params');
+
+initParamsSetButton.addEventListener('click', event => {
+    if(initParamsSetButton.classList.contains('disabled')){
+        return ;
+    }
+    editor.classList.toggle('open');
+    const editorTextArea = document.querySelector('.codeflask__textarea');
+    editorTextArea.focus();
+});
+closeEditor.addEventListener('click', event => {
+    editor.classList.toggle('open');
+});
+saveParams.addEventListener('click', event => {
+    editor.classList.toggle('open');
+});
 
 // set clip container's dimensions
 clipContainer.style.width = clip.props.containerParams.width;
