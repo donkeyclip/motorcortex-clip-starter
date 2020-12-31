b3221e5a41287dbab87b8e7548d4bcb0a3d3400b
andread-trad <atrantidis@gmail.com>
2020-12-31 09:54:32 +0200

    progress

--- demo/code-dc/scripts/elements.js
+++
@@ -1,6 +0,0 @@
-export const toolset = document.getElementById('motorcortex-tools');
-export const editor = document.getElementById('json-editor');
-export const closeEditor = document.getElementById('cancel-init-params');
-export const saveParams = document.getElementById('save-init-params');
-export const initParamsSetButton = document.getElementById('set-init-params');
-export const errorContainer = document.getElementById('error-container');
