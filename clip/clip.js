45ced0d12510f9991a6471213734b09863b5d7a9
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-03-19 11:01:03 +0200

    fix: log the right error on call fail

--- clip/clip.js
+++ clip/clip.js
@@ -1,23 +1,23 @@
-import MotorCortex from '@kissmybutton/motorcortex';
-import clipStyle from './style.css';
+import MotorCortex from "@kissmybutton/motorcortex";
+import clipStyle from "./style.css";
 
 export const clip = new MotorCortex.HTMLClip({
-    html: `<div class="container">
+  html: `<div class="container">
         Welcome!
         <br/><br/>
         You've picked <u><%= initParams.color %></u>
     </div>`,
-    css: clipStyle.toString(),
-    host: document.getElementById('clip'),
-    containerParams: {
-        width: '800px',
-        height: '675px'
-    },
-    initParams: {
-        color: "white" // eventhough we expect color's value on our initParams, still "white" will act as the default
-    }
+  css: clipStyle.toString(),
+  host: document.getElementById("clip"),
+  containerParams: {
+    width: "800px",
+    height: "675px",
+  },
+  initParams: {
+    color: "white", // eventhough we expect color's value on our initParams, still "white" will act as the default
+  },
 });
 
 export const initParamsMap = {
-    color: [`${clip.id}.props.initParams.color`]
-}
+  color: [`${clip.id}.props.initParams.color`],
+};
