a5b7c3414e7b44c472e251499313a0638283dc09
Alexandros Dorodoulis <aldor94@gmail.com>
2021-06-16 20:23:30 +0200

    refactor: drop whitespace, early return

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,8 @@
-import { clip } from "../clip/clip";
-import Player from "@kissmybutton/motorcortex-player";
-import initParamsApply from "./scripts/initParamsApply";
 import { utils } from "@kissmybutton/motorcortex";
+import Player from "@kissmybutton/motorcortex-player";
+import { clip } from "../clip/clip";
 import clipId from "../clip/id";
+import initParamsApply from "./scripts/initParamsApply";
 
 const liveDef = clip.exportLiveDefinition();
 liveDef.props.id = clip.id;
@@ -19,25 +19,24 @@ window.top.postMessage(
 );
 
 window.addEventListener("message", (event) => {
-  if (event.data.what === "initParamsChange") {
-    const newLiveDef = initParamsApply(
-      liveDef,
-      event.data.initParams
-    );
-    document.getElementById("projector").innerHTML = "<div id='clip'></div>";
-    const clipContainer = document.getElementById("clip");
-    // set clip container's dimensions
-    clipContainer.style.width = clip.props.containerParams.width;
-    clipContainer.style.height = clip.props.containerParams.height;
-    newLiveDef.props.host = clipContainer;
-    const newclip = utils.clipFromDefinition(newLiveDef);
-    window.mc = { Player: new Player({ clip: newclip }) };
+  if (event.data.what !== "initParamsChange") {
+    return;
   }
+
+  const newLiveDef = initParamsApply(liveDef, event.data.initParams);
+  document.getElementById("projector").innerHTML = "<div id='clip'></div>";
+  const clipContainer = document.getElementById("clip");
+  // set clip container's dimensions
+  clipContainer.style.width = clip.props.containerParams.width;
+  clipContainer.style.height = clip.props.containerParams.height;
+  newLiveDef.props.host = clipContainer;
+  const newclip = utils.clipFromDefinition(newLiveDef);
+  window.mc = { Player: new Player({ clip: newclip }) };
 });
 
-const clipContainer = document.getElementById("clip");
+const clipElement = document.getElementById("clip");
 // set clip container's dimensions
-clipContainer.style.width = clip.props.containerParams.width;
-clipContainer.style.height = clip.props.containerParams.height;
+clipElement.style.width = clip.props.containerParams.width;
+clipElement.style.height = clip.props.containerParams.height;
 
 window.mc = { Player: new Player({ clip }) };
